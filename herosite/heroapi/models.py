a4ae589da90f15374b902374bb7c125a24a418b3
p0lycarpio <arsene.reymond@free.fr>
2021-09-15 11:24:57 +0200

    add models

--- herosite/heroapi/models.py
+++ herosite/heroapi/models.py
@@ -2,8 +2,22 @@
 
 from django.db import models
 
+class Publisher(models.Model):
+    creator = models.CharField(max_length=80)
+    def __str__(self):
+        return self.creator
+
 class Hero(models.Model):
     name = models.CharField(max_length=80)
     alias = models.CharField(max_length=80)
+    publisher = models.ForeignKey(Publisher, default=None, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+
+class SuperPower(models.Model):
+    name = models.CharField(max_length=80)
+    heroes = models.ManyToManyField(Hero)
     def __str__(self):
         return self.name
--- herosite/heroapi/serializers.py
+++ herosite/heroapi/serializers.py
@@ -8,4 +8,4 @@ from .models import Hero
 class HeroSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Hero
-        fields = ('name', 'alias')
+        fields = ('id','name', 'alias')
